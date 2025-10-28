8eacc074653709c35f4b7f5f1f2bcfabbd89b4fe
Eduardo Piquer Sanchez <eduardo.piquer.sanchez@ces-vegamedia.es>
2025-10-28 21:41:32 +0100

    Modificada database

--- pokedaw.sql
+++ pokedaw.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2025 a las 10:38:02
+-- Tiempo de generación: 28-10-2025 a las 21:40:13
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,13 +26,32 @@ USE `pokedaw`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pokemons`
+--
+
+DROP TABLE IF EXISTS `pokemons`;
+CREATE TABLE `pokemons` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `usuario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
+DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
-  `email` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL,
+  `nif` int(10) NOT NULL,
   `nombre` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `direccion` varchar(250) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `rol` int(11) NOT NULL COMMENT '0->Normal\r\n1-->admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -40,19 +59,47 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`email`, `pass`, `nombre`, `rol`) VALUES
-('admin@pokedaw.com', '1234', 'Fernando', 1),
-('normal@pokedaw.com', '1234', 'Mini Fer', 0);
+INSERT INTO `usuarios` (`nif`, `nombre`, `apellidos`, `direccion`, `email`, `telefono`, `pass`, `rol`) VALUES
+(1, 'Fernando', '', '', 'admin@pokedaw.com', '', '1234', 1),
+(2, 'Mini Fer', '', '', 'normal@pokedaw.com', '', '1234', 0);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `pokemons`
+--
+ALTER TABLE `pokemons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario` (`usuario`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`nif`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pokemons`
+--
+ALTER TABLE `pokemons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pokemons`
+--
+ALTER TABLE `pokemons`
+  ADD CONSTRAINT `pokemons_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`nif`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
