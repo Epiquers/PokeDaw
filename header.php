2bb5f6b66e0b1781384d4d36fa1fe584818da653
Eduardo Piquer Sanchez <eduardo.piquer.sanchez@ces-vegamedia.es>
2025-10-28 11:52:51 +0100

    Comienzo de proyecto Cambio de colores

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class="container-fluid cabecera">
+    <header class="row justify-content-center align-items-center">
+        <div class="d-none d-md-block col-4  p-0 izq mt-4"></div>
+        <div class="col-12 col-md-4">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-auto mt-1">
+                    <img class="m-0" src="img/pokeball.png" style="width:50px">
+                </div>
+                <div class="col-auto">
+                    <p class="text-center m-0"><span class="v"> V</span><span class="me">M</span></p>
+                </div>
+                <div class="col-auto mt-1 ms-4"></div>
+                <div class="w-100"></div>
+            </div>
+        </div>
+        <div class="d-none d-md-block col-4 p-0 der mt-4"></div>
+        <div class="col-12 mb-1">
+            <p class="h1 text-center">Colección de Cartas Pokemon</p>
+        </div>
+    </header>
+</div>
